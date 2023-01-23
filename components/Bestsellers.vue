1e54138c11c476858f220e1614a7868927faa6b4
Dan <danieldawes@protonmail.com>
2023-01-23 09:43:44 +0000

    bestsellers updated

--- components/Bestsellers.vue
+++ components/Bestsellers.vue
@@ -13,185 +13,73 @@
       <div class="hidden lg:grid grid-cols-4 grid-rows-2 gap-2">
         <!--        bestseller-->
         <Bestseller name="ullamcorper malesuada proin"
-                    price="12.00"
+                    price=12
                     />
         <!--        bestseller-->
         <Bestseller name="in fermentum et"
-                    price="12.00"
+                    price=12
         />
         <!--        bestseller-->
-        <div class="inline-block">
-          <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-          </div>
-          <div class="flex flex-col mt-2 space-y-4">
-            <span class="block text-[#3c081b] font-body text-md capitalize">ultrices tincidunt arcu</span>
-            <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-          </div>
-        </div>
+        <Bestseller name="ultrices tincidunt arcu"
+                    price=12
+        />
         <!--        bestseller-->
-        <div class="inline-block">
-          <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-          </div>
-          <div class="flex flex-col mt-2 space-y-4">
-            <span class="block text-[#3c081b] font-body text-md capitalize">mus mauris vitae</span>
-            <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-          </div>
-        </div>
+        <Bestseller name="mus mauris vitae"
+                    price=12
+        />
         <!--        bestseller-->
-        <div class="inline-block">
-          <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-          </div>
-          <div class="flex flex-col mt-2 space-y-4">
-            <span class="block text-[#3c081b] font-body text-md capitalize">aliquam purus sit</span>
-            <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-          </div>
-        </div>
+        <Bestseller name="ullamcorper malesuada proin"
+                    price=12
+        />
         <!--        bestseller-->
-        <div class="inline-block">
-          <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-          </div>
-          <div class="flex flex-col mt-2 space-y-4">
-            <span class="block text-[#3c081b] font-body text-md capitalize">placerat orci nulla</span>
-            <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-          </div>
-        </div>
+        <Bestseller name="in fermentum et"
+                    price=12
+        />
         <!--        bestseller-->
-        <div class="inline-block">
-          <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-          </div>
-          <div class="flex flex-col mt-2 space-y-4">
-            <span class="block text-[#3c081b] font-body text-md capitalize">in massa tempor</span>
-            <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-          </div>
-        </div>
+        <Bestseller name="ultrices tincidunt arcu"
+                    price=12
+        />
         <!--        bestseller-->
-        <div class="inline-block">
-          <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-          </div>
-          <div class="flex flex-col mt-2 space-y-4">
-            <span class="block text-[#3c081b] font-body text-md capitalize">massa sed elementum</span>
-            <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-          </div>
-        </div>
+        <Bestseller name="mus mauris vitae"
+                    price=12
+        />
       </div>
 
       <!--      bestsellers for small screens-->
       <div class="lg:hidden flex overflow-scroll max-w-6xl mx-auto h-full no-scrollbar">
         <div class="flex flex-nowrap md:ml-6 ml-3 space-x-2">
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">ullamcorper malesuada proin</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="ullamcorper malesuada proin"
+                      price=12
+          />
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">in fermentum et</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="in fermentum et"
+                      price=12
+          />
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">ultrices tincidunt arcu</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="ultrices tincidunt arcu"
+                      price=12
+          />
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">mus mauris vitae</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="mus mauris vitae"
+                      price=12
+          />
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">aliquam purus sit</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="ullamcorper malesuada proin"
+                      price=12
+          />
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">placerat orci nulla</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="in fermentum et"
+                      price=12
+          />
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">in massa tempor</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="ultrices tincidunt arcu"
+                      price=12
+          />
           <!--        bestseller-->
-          <div class="inline-block">
-            <div class="text-[#3c081b] w-64 h-64 grid place-items-center max-w-xs overflow-hidden rounded-2xl shadow-md bg-yellow-50 border-x-2 border-x-[#3c081b] border-t-2 border-t-[#3c081b] border-b-8 border-b-[#3c081b] hover:shadow-xl transition-shadow duration-300 ease-in-out">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-            </div>
-            <div class="flex flex-col mt-2 space-y-4">
-              <span class="block text-[#3c081b] font-body text-md capitalize">massa sed elementum</span>
-              <span class="text-[#3c081b] font-sans text-md tracking-wider">£12.00</span>
-            </div>
-          </div>
+          <Bestseller name="mus mauris vitae"
+                      price=12
+          />
         </div>
       </div>
     </div>
